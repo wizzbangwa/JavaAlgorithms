12e54d2d0d6bb260985ffeafdcab783ca54a598d
Wizbangwa <wizzbangwa@gmail.com>
2024-04-11 11:58:32 -0700

    Add selection for CLI or GUI.

--- app/src/main/java/javaalgorithms/App.java
+++ app/src/main/java/javaalgorithms/App.java
@@ -8,21 +8,44 @@ import java.util.Scanner;
 
 public class App {
     
+    private static int _maxInputLength = 255;
+    private static final String GUI_STRING = "gui";
+    private static final String CLI_STRING = "cli";
+    private static Scanner _scanner;
+
     public static void main(String[] args) {
-        var maxInputLength = 255;
+        String uiType = CLI_STRING;
+        _scanner = new Scanner(System.in);
+
+        if (args != null && args.length > 0) {
+            uiType = args[0];
+        } else {
+            uiType = GetUIChoiceFromUser();
+        }
+
+        if (uiType == CLI_STRING) {
+            ShowCLI();
+        } else {
+            ShowGUI();
+        }
+        _scanner.close();
+    }
+
+    private static void ShowCLI()
+    {
         String userInput = null;
-        Scanner scanner = new Scanner(System.in);
+
         while (true)
         {
-            System.out.println("Please enter a string up to " + maxInputLength + " characters in length.  Numbers, spaces, and alpha characters are ok.\r\n");
+            System.out.println("Please enter a string up to " + _maxInputLength + " characters in length.  Numbers, spaces, and alpha characters are ok.\r\n");
             
-            userInput = scanner.nextLine();
+            userInput = _scanner.nextLine();
 
             // Check if input exceeds maximum length
-            if (userInput.length() > maxInputLength) {
-                System.out.println("\r\nInput exceeds maximum length of " + maxInputLength + " characters.");
+            if (userInput.length() > _maxInputLength) {
+                System.out.println("\r\nInput exceeds maximum length of " + _maxInputLength + " characters.");
                 // Truncate input to maximum length
-                userInput = userInput.substring(0, maxInputLength);
+                userInput = userInput.substring(0, _maxInputLength);
                 System.out.println("\r\nTruncated string: " + userInput + "\r\n");
             }
 
@@ -39,7 +62,43 @@ public class App {
                 System.out.println("\r\nNumber of vowels: " + CountVowels(userInput));
             }
         }
-        scanner.close();
+    }
+
+    private static void ShowGUI()
+    {
+        System.out.println("GUI has not been setup yet.");
+    }
+
+    private static String GetUIChoiceFromUser()
+    {
+        String uiSelection = "";
+
+        String prompt = "Select a user interface type to continue.\r\n" +
+            "HINT: passing either \"cli\" or \"gui\" as a command line argument will bypass this selection.\r\n\r\n" +
+            "1. Command Line Interface (CLI)\r\n" +
+            "2. Graphical User Interface (GUI)\r\n\r\n" +
+            "Select an interface to use.\r\n";
+
+        System.out.println(prompt);
+
+        while (uiSelection == "") {
+            System.out.print("");
+            String selection = _scanner.nextLine();
+            if (!selection.isEmpty()) {
+                selection = selection.toLowerCase();
+                if (selection.equals("1")) {
+                    uiSelection = CLI_STRING;
+                    break;
+                } else if (selection.equals("2")) {
+                    uiSelection = GUI_STRING;
+                    break;
+                } else {
+                    System.out.println("\r\nInvalid selection. Press 1 or 2 to select a UI type.\r\n");
+                }
+            }
+        }
+
+        return uiSelection;
     }
 
     private static int CountVowels(String sampleString)
